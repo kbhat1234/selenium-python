e470447913d3f9377ca05c1a48217033cbf4088c
kbhat1234 <karthikbhat@live.com>
2025-05-05 13:57:45 +0530

    add

---
+++ Strings/String7.py
@@ -0,0 +1,25 @@
+str1 = "Welcome to python"
+print(len(str1))
+print(len("welcome"))
+print(str1[0])
+print(str1[-1])
+print(str1[1:4])
+print(str1[1:])
+print(str1[:4])
+print(str1[:])
+
+# \'
+str2 = "welcome to 'python' programming"
+print(str2)
+
+#\"
+str3 = 'Welcome to "python" programming'
+print(str3)
+
+# \\
+str4 = "Hello \\world"
+print(str4)
+
+# \n
+str5 = "Welcome to \nPython \nProgramming"
+print(str5)
